ff334d4a0a5948e9609aaf54580bd8452ea43def
tiantian <1500846601@qq.com>
2019-06-16 21:40:46 +0800

    like 组件 properties 学习

--- component/like/index.js
+++ component/like/index.js
@@ -1,9 +1,28 @@
 Component({
-  properties: {},
-  data: {},
+  properties: {
+    like: {
+      type: Boolean
+    },
+    count: {
+      type: Number
+    }
+  },
+  data: {
+    yesSrc:'images/like.png',
+    noSrc: 'images/unLike.png'
+  },
   methods: {
     onLike(event) {
-      console.log(1)
+      let like = this.properties.like
+      let count = this.properties.count
+
+      //初始值 like 为 false，当我点击的时候，like 为 false，此时 count 要加 1，然后在设置 like 为 true
+      count = like ? count-1 : count+1
+
+      this.setData({
+        count: count,
+        like: !like
+      })
     }
   }
 })
